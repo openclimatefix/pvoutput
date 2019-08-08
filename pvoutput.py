b84733867e77e4bc41c1badfb0762ac20511568f
Jack Kelly <jack-list@xlk.org.uk>
2019-08-08 13:20:02 +0100

    Added Python type annotations.  #6

--- pvoutput.py
+++ pvoutput.py
@@ -24,6 +24,7 @@ import requests
 from requests.adapters import HTTPAdapter
 import numpy as np
 import pandas as pd
+from typing import Dict, Union
 
 SECONDS_PER_DAY = 60 * 60 * 24
 ONE_DAY = timedelta(days=1)
@@ -55,11 +56,11 @@ _logger = get_logger()
 
 
 class BadStatusCode(Exception):
-    def __init__(self, response, message=''):
+    def __init__(self, response: requests.Response, message: str = ''):
         self.response = response
         super(BadStatusCode, self).__init__(message)
         
-    def __str__(self):
+    def __str__(self) -> str:
         string = super(BadStatusCode, self).__str__()
         string += "Status code returned: {}\n".format(self.response.status_code)
         string += "Response content: {}\n".format(self.response.content)
@@ -84,17 +85,17 @@ class RateLimitExceeded(BadStatusCode):
         self.timedelta_to_wait += timedelta(minutes=3) # Just for safety
         self.secs_to_wait = self.timedelta_to_wait.total_seconds()
         
-    def __str__(self):
+    def __str__(self) -> str:
         return 'Rate limit exceeded!'
     
-    def wait_message(self):
+    def wait_message(self) -> str:
         retry_time_utc = self.utc_now + self.timedelta_to_wait
         return '{}  Waiting {:.0f} seconds.  Will retry at {} UTC.'.format(
             self, self.secs_to_wait,
             retry_time_utc.strftime('%Y-%m-%d %H:%M:%S'))
 
 
-def _get_session_with_retry():
+def _get_session_with_retry() -> requests.Session:
     session = requests.Session()
     max_retry_counts = dict(
         connect=720,  # How many connection-related errors to retry on.
@@ -114,7 +115,7 @@ def _get_session_with_retry():
     return session
 
 
-def _get_api_reponse(service, api_params):
+def _get_api_reponse(service: str, api_params: Dict) -> requests.Response:
     """
     Args:
         service: string, e.g. 'search', 'getstatus'
@@ -141,7 +142,20 @@ def _get_api_reponse(service, api_params):
     return response
 
 
-def _process_api_response(response):
+def _process_api_response(response: requests.Response) -> str:
+    """Turns an API response into text.
+    
+    Args:
+        response: from _get_api_reponse()
+    
+    Returns:
+        content of the response.
+
+    Raises:
+        UnicodeDecodeError
+        NoStatusFound
+        RateLimitExceeded
+    """
     try:
         content = response.content.decode('latin1').strip()
     except UnicodeDecodeError as e:
@@ -164,12 +178,17 @@ def _process_api_response(response):
     return content
 
 
-def pv_output_api_query(service, api_params, wait_if_rate_limit_exceeded=True):
-    """
+def pv_output_api_query(service: str,
+                        api_params: Dict,
+                        wait_if_rate_limit_exceeded: bool = True
+                        ) -> str:
+    """Send API request to PVOutput.org and return content text.
+
     Args:
         service: string, e.g. 'search' or 'getstatus'
         api_params: dict
         wait_if_rate_limit_exceeded: bool
+
     Raises:
         NoStatusFound
         RateLimitExceeded
@@ -192,15 +211,36 @@ def pv_output_api_query(service, api_params, wait_if_rate_limit_exceeded=True):
             raise
 
 
-def pv_system_search(query, lat_lon, **kwargs):
-    """
+def pv_system_search(query: str, lat_lon: str, **kwargs) -> pd.DataFrame:
+    """Send a search query to PVOutput.org.
+
+    Some quirks of the PVOutput.org API:
+        - The maximum number of results returned by PVOutput.org is 30.
+            If the number of returned results is 30, then there is indication
+            of whether there are exactly 30 search results, or if there
+            are more than 30.  Also, there is no way to request additional
+            'pages' of search results.
+        - The maximum search radius is 25km
+
     Args:
         query: string, see https://pvoutput.org/help.html#search
-            e.g. '5km'.  Max search radius = 25km
+            e.g. '5km'.
         lat_lon: string, e.g. '52.0668589,-1.3484038'
         
     Returns:
-        pd.DataFrame.  Beware the maximum number of results returned by PVOutput.org is 30.
+        pd.DataFrame, one row per search results.  Index is PV system ID (int).
+            Columns:
+                system_name,
+                system_size_watts,
+                postcode,  # including the country
+                orientation,
+                num_outputs,
+                last_output,
+                panel,
+                inverter,
+                distance_km,
+                latitude,
+                longitude
     """
     
     pv_systems_text = pv_output_api_query(
@@ -231,14 +271,21 @@ def pv_system_search(query, lat_lon, **kwargs):
     return pv_systems
 
 
-def date_to_pvoutput_str(date):
+def date_to_pvoutput_str(date: Union[str, datetime]) -> str:
+    """Convert datetime to date string for PVOutput.org in YYYYMMDD format."""
     if isinstance(date, str):
         return date
     else:
         return date.strftime(PV_OUTPUT_DATE_FORMAT)
 
 
-def _check_date(date):
+def _check_date(date: str):
+    """Check that date string conforms to YYYYMMDD format, 
+    and that the date isn't in the future.
+
+    Raises:
+        ValueError if the date is 'bad'.
+    """
     dt = datetime.strptime(date, PV_OUTPUT_DATE_FORMAT)
     if dt > datetime.now():
         raise ValueError(
@@ -246,7 +293,10 @@ def _check_date(date):
             .format(date, datetime.now()))
         
 
-def get_pv_system_status(pv_system_id, date, **kwargs):
+def get_pv_system_status(pv_system_id: int,
+                         date: str,
+                         **kwargs
+                         ) -> pd.DataFrame:
     """Get PV system status (e.g. instantaneous power generation) for one day.
 
     Args:
@@ -269,7 +319,7 @@ def get_pv_system_status(pv_system_id, date, **kwargs):
     """
     date = date_to_pvoutput_str(date)
     _check_date(date)
-    
+
     pv_system_status_text = pv_output_api_query(
         service='getstatus',
         api_params={
@@ -304,7 +354,17 @@ def get_pv_system_status(pv_system_id, date, **kwargs):
     return pv_system_status
 
 
-def check_pv_system_status(pv_system_status, requested_date_str):
+def check_pv_system_status(pv_system_status: pd.DataFrame,
+                           requested_date_str: str):
+    """Checks the DataFrame returned by get_pv_system_status.
+
+    Args:
+        pv_system_status: DataFrame returned by get_pv_system_status
+        requested_date_str: Date string in YYYYMMDD format.
+
+    Raises:
+        ValueError if the DataFrame is incorrect.
+    """
     if not isinstance(pv_system_status, pd.DataFrame):
         raise ValueError('pv_system_status must be a dataframe')
     requested_date = datetime.strptime(requested_date_str, "%Y%m%d").date()
@@ -318,10 +378,36 @@ def check_pv_system_status(pv_system_status, requested_date_str):
                       .format(d, requested_date_str))
 
 
-def get_pv_metadata(pv_system_id, **kwargs):
-    """
+def get_pv_metadata(pv_system_id: int, **kwargs) -> pd.Series:
+    """Calls PVOutput.org's 'getsystem' service to get metadata for a single PV
+    system.
+
     Args:
         pv_system_id: int
+
+    Returns:
+        pd.Series.  Index is:
+            system_name,
+            system_id,
+            system_size_watts,
+            postcode,
+            number_of_panels,
+            panel_power_watts,
+            panel_brand,
+            num_inverters,
+            inverter_power_watts,
+            inverter_brand,
+            orientation,
+            array_tilt_degrees,
+            shade,
+            install_date,
+            latitude,
+            longitude,
+            status_interval_minutes,
+            number_of_panels_secondary,
+            panel_power_watts_secondary,
+            orientation_secondary,
+            array_tilt_degrees_secondary
     """
     pv_metadata_text = pv_output_api_query(
         service='getsystem',
@@ -369,7 +455,29 @@ def get_pv_metadata(pv_system_id, **kwargs):
     return pv_metadata
 
 
-def get_pv_statistic(pv_system_id, **kwargs):
+def get_pv_statistic(pv_system_id: int, **kwargs) -> pd.Series:
+    """Calls PVOutput.org's 'getstatistic' service to get summary stats for a 
+    single PV system
+    
+    Args:
+        pv_system_id: int
+
+    Returns:
+        pd.Series with index:
+            energy_generated_Wh,
+            energy_exported_Wh,
+            average_generation_Wh,
+            minimum_generation_Wh,
+            maximum_generation_Wh,
+            average_efficiency_kWh_per_kW,
+            outputs,  # The total number of data outputs recorded by PVOutput
+            actual_date_from,
+            actual_date_to,
+            record_efficiency_kWh_per_kW,
+            record_efficiency_date,
+            system_id
+    """
+    
     pv_metadata_text = pv_output_api_query(
         service='getstatistic',
         api_params={
@@ -394,7 +502,11 @@ def get_pv_statistic(pv_system_id, **kwargs):
             'record_efficiency_kWh_per_kW',
             'record_efficiency_date'
         ],
-        parse_dates=['actual_date_from', 'actual_date_to', 'record_efficiency_date']
+        parse_dates=[
+            'actual_date_from',
+            'actual_date_to',
+            'record_efficiency_date'
+        ]
     ).squeeze()
     pv_metadata['system_id'] = pv_system_id
     pv_metadata.name = pv_system_id
