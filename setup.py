2d647a065dda7b0295b39238bd0b5752cf72a251
peterdudfield <peter.dudfield@hotmail.com>
2022-02-18 09:13:26 +0000

    update setup

--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@ from setuptools import find_packages, setup
 
 this_directory = Path(__file__).parent
 install_requires = (this_directory / "requirements.txt").read_text().splitlines()
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pvoutput-ocf",
     version="0.1.2",
+    license="MIT",
     packages=find_packages(),
     install_requires=install_requires,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    company="Open Climate Fix Ltd",
+    author_email="info@openclimatefix.org",
 )
