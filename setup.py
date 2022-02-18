ef91e6c6d2372cad09ab966ee43ba10ca5a961c7
BumpVersion Action <bumpversion@github-actions>
2022-02-18 08:40:47 +0000

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ install_requires = (this_directory / "requirements.txt").read_text().splitlines(
 
 setup(
     name="pvoutput",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=install_requires,
 )
