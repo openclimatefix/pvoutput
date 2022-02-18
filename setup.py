24ac2fc9d11ad34f92f81ba14cc1e846322d30df
peterdudfield <peter.dudfield@hotmail.com>
2022-02-18 09:10:07 +0000

    packagename pvoutput-ocf

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = Path(__file__).parent
 install_requires = (this_directory / "requirements.txt").read_text().splitlines()
 
 setup(
-    name="pvoutput",
+    name="pvoutput-ocf",
     version="0.1.2",
     packages=find_packages(),
     install_requires=install_requires,
