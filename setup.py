dba9a696cbd302a637a84120c28d151cdac964bf
BumpVersion Action <bumpversion@github-actions>
2022-05-04 19:38:21 +0000

    Bump version: 0.1.18 → 0.1.19

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="pvoutput-ocf",
-    version="0.1.18",
+    version="0.1.19",
     license="MIT",
     packages=find_packages(),
     install_requires=install_requires,
